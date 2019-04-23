4e9cb17ab7e59d2f91ded1031c2c703ecb56cdbc
Pratyush Srivastava <pratyush272@gmail.com>
2019-04-23 05:44:06 -0400

    Update GraphsAndVisualizations_colors_ggplot_ggmap.R

--- GraphsAndVisualizations_colors_ggplot_ggmap.R
+++ GraphsAndVisualizations_colors_ggplot_ggmap.R
@@ -6,3 +6,9 @@
         rbPal <- colorRampPalette(c('blue','yellow','red'),alpha=.8)
         cl <- rbPal(10)[as.numeric(cut(iris$Petal.Width,breaks = 10))]
         plot(iris$Petal.Length,iris$Petal.Width,col=cl)
+
+
+#####
+# MAPS
+#####
+leaflet() %>% addTiles() %>% addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
