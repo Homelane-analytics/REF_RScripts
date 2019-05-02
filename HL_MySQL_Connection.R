dc5f532089d6d45aafce01bace1725360de1f6c6
Pratyush Srivastava <pratyush272@gmail.com>
2019-05-02 15:32:21 +0530

    Update HL_MySQL_Connection.R

--- HL_MySQL_Connection.R
+++ HL_MySQL_Connection.R
@@ -1,17 +1,24 @@
+
 install.packages("RMySQL")
 library(RMySQL)
+library(googlesheets)
 
+
+gs_ls()
 
 # ALTER USER 'root'@'localhost'
 # IDENTIFIED WITH mysql_native_password BY 'tt089'
 
-db<- dbConnect(MySQL(), user='root', password= 'xxx', dbname='ps_hl_1', host='localhost')
+db<- dbConnect(MySQL(), user='root', password= 'ttoagk089', dbname='ps_hl_1', host='localhost')
 
 
-dbListTables(mydb)
+dbListTables(db)
 
-dbListFields(mydb, 'some_table')
+dbListFields(db, 'aux_customer')
 
-dbSendQuery(mydb, 'drop table if exists some_table, some_other_table')
+a<- dbSendQuery(db, 'select  * from aux_customer limit 100')
+rs = fetch(dbSendQuery(db, "select * from aux_customer"),n=-1)
 
-dbWriteTable(mydb, name='table_name', value=data.frame.name)
+# SET GLOBAL local_infile = true;
+custs<- gs_read(gs_title("PS HL Sales DB"),ws="AUX_Cust")
+dbWriteTable(db, name='AUX_Customers3', value=custs)
